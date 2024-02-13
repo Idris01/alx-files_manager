329de15be08d5fc20b3a62dce39f50b9310c44e1
Idris Adebowale <idrys01@gmail.com>
2024-02-13 06:43:19 -0800

    fix code linting

--- utils/redis.js
+++ utils/redis.js
@@ -2,10 +2,10 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.isActive = true;
     this.client = createClient();
-    this.client.on('error', error => console.log(error));
+    this.client.on('error', (error) => console.log(error));
     this.client.on('ready', () => {
       this.isActive = true;
     });
@@ -19,25 +19,25 @@ class RedisClient {
     });
   }
 
-  isAlive () {
+  isAlive() {
     // Check if the client is connected
     return this.isActive;
   }
 
-  async get (key) {
+  async get(key) {
     // Use promisify to convert callback-based function to promise-based
     const getAsync = promisify(this.client.get).bind(this.client);
     const data = await getAsync(key);
     return data;
   }
 
-  async set (key, val, duration) {
+  async set(key, val, duration) {
     // Use promisify to convert callback-based function to promise-based
     const setAsync = promisify(this.client.set).bind(this.client);
     await setAsync(key, val, 'EX', duration); // 'EX' specifies the expiration time in seconds
   }
 
-  async del (key) {
+  async del(key) {
     // Use promisify to convert callback-based function to promise-based
     const delAsync = promisify(this.client.del).bind(this.client);
     await delAsync(key);
